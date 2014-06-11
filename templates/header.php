ab9d98f14491b120a59bdd697d712496ba0f7454
Walid <walidvb@gmail.com>
2014-06-12 01:04:51 +0200

    adds bootstrap navbar

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,23 @@
-<ul class="menu1">
-<li><div>
-<a class="menu1item" href="//bamfestival.be/index.html">Accueil</a></div>
-</li><li><div class="divider menu1divider"></div><div>
-<a class="menu1item" href="//bamfestival.be/a-propos.html">À propos</a></div>
-</li><li><div class="divider menu1divider"></div><div>
-<a class="menu1item" href="//bamfestival.be/programmation.html">Programmation</a></div>
-</li><li><div class="divider menu1divider"></div><div>
-<a class="menu1item menu1selected" href="//bamfestival.be/artistes.html">Artistes</a></div>
-</li><li><div class="divider menu1divider"></div><div>
-<a class="menu1item" href="//bamfestival.be/media.html">Media</a></div>
-</li><li><div class="divider menu1divider"></div><div>
-<a class="menu1item" href="//bamfestival.be/infos.html">Infos</a></div>
-</li></ul>
+<nav class="navbar navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="/?q=accueil">Accueil</a></li>
+					<li><a href="/?q=a-propos">À Propos</a></li>
+					<li><a href="/?q=programmation">Programmation</a></li>
+					<li><a href="/?q=artistes">Artistes</a></li>
+					<li><a href="/?q=media">Média</a></li>
+					<li><a href="/?q=infos">Infos</a></li>
+		</ul>	
+	</div><!-- /.navbar-collapse -->
+</nav>
